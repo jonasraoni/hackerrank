a4720c04f76979fb9d2dbf7d13b1ec2744aba240
Jonas Raoni Soares da Silva <jonas@raoni.org>
2021-02-13 11:48:28 +0300

    Solved Luck Balance and Minimum Absolute Difference in an Array

---
+++ minimumAbsoluteDifference.js
@@ -0,0 +1,11 @@
+//+ Jonas Raoni Soares Silva
+//@ http://raoni.org
+
+function minimumAbsoluteDifference(arr) {
+  // After sorting, the numbers will be closer to each other, which is the best outcome for the modulo (e.g. 99 - 99 or 0 - 0)
+  const [a, b] = arr.sort((a, b) => a - b).slice(-2);
+  let min = Math.abs(a - b);
+  for (let i = arr.length - 3; i-- > 0; )
+    min = Math.min(min, Math.abs(arr[i] - arr[i + 1]));
+  return min;
+}
